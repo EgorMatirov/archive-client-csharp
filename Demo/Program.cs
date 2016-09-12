29c9973779dd0696f50a363e73f09caa908d77f9
Mikhail Ivchenko <ematirov@gmail.com>
2016-09-12 19:59:25 +0400

    Initial commit

---
+++ Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Bacs.Archive;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace Demo
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const string host = "hostname:port";
+            var credentials = new SslCredentials("");
+            var client = new Archive.ArchiveClient(new Channel(host, credentials));
+            var result = client.ExistingAll(new Empty());
+            foreach (var id in result.Id)
+            {
+                Console.WriteLine(id);
+            }
+            Console.ReadLine();
+        }
+    }
+}
